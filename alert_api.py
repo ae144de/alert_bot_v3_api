f98617534579db505d35936981cd648e6f15b7dc
rkerem <recep.kerem1d@gmail.com>
2024-12-22 06:30:16 +0300

    Fixing token_required -6

--- alert_api.py
+++ alert_api.py
@@ -53,6 +53,7 @@ def token_required(f):
             bearer = request.headers['Authorization']
             print(f"Bearer: {bearer}")
             if bearer and bearer.startswith('Bearer '):
+                print("There is a Bearer and starts with Bearer !!!")
                 token = bearer.split(' ')[1]
                 print(f"**TOKEN: {token}")
 
