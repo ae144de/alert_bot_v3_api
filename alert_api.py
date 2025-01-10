7d2dbc27d97feda5066e15dc7aa046ccd156eb9b
rkerem <recep.kerem1d@gmail.com>
2025-01-10 22:40:28 +0300

    Fix previous price retrieval in evaluate_condition to use get method for safer access

--- alert_api.py
+++ alert_api.py
@@ -266,7 +266,7 @@ def evaluate_condition(price, threshold, operator, symbol, lower_bound=None, upp
     # channel: The channel range
 
     global previous_close_prices
-    previous_price = previous_close_prices[symbol]
+    previous_price = previous_close_prices.get(symbol)
     print(Back.LIGHTGREEN_EX + f"PREVIOUS PRICE: {previous_price} --- CURRENT PRICE: {price} --- THRESHOLD: {threshold} --- OPERATOR: {operator}")
     print(Back.BLACK + f"Previous close prices: {previous_close_prices}")
     
