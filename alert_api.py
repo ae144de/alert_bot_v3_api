df836a9b78cd527bd1b0617ccccda422acf7abd9
rkerem <recep.kerem1d@gmail.com>
2025-01-11 00:21:35 +0300

    Restore debug print statement for previous price in evaluate_condition for better visibility

--- alert_api.py
+++ alert_api.py
@@ -268,7 +268,7 @@ def evaluate_condition(price, threshold, operator, symbol, lower_bound=None, upp
     global previous_close_prices
     # previous_close_prices[symbol] = previous_close_prices.get(symbol)
     previous_price = previous_close_prices.get(symbol)
-    # print(Back.LIGHTGREEN_EX + f"PREVIOUS PRICE: {previous_close_prices[symbol]} --- CURRENT PRICE: {price} --- THRESHOLD: {threshold} --- OPERATOR: {operator}")
+    print(Back.LIGHTGREEN_EX + f"PREVIOUS PRICE: {previous_close_prices[symbol]} --- CURRENT PRICE: {price} --- THRESHOLD: {threshold} --- OPERATOR: {operator}")
     print(Back.BLACK + f"Previous close prices: {previous_close_prices}")
     
     if previous_price is None:
